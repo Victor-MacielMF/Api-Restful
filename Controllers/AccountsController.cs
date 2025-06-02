5eef7d219ab56aa83fb15289b8f9e1f4e401ecda
Victor-MacielMF <joaovictormacieldefreitas@gmail.com>
2025-06-02 14:42:40 -0300

    refactor: standardize responses of AccountsController endpoints

--- Controllers/AccountsController.cs
+++ Controllers/AccountsController.cs
@@ -1,3 +1,4 @@
+using api.Dtos;
 using api.Dtos.Account;
 using api.Interfaces;
 using api.Mappers;
@@ -17,16 +18,13 @@ namespace api.Controllers
 
         [HttpPost("register")]
         [Produces("application/json")]
-        [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
-        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(DataResponse<AccountDto>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(DataResponse<IEnumerable<string>>), StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(typeof(MessageResponse), StatusCodes.Status500InternalServerError)]
         public async Task<IActionResult> Register([FromBody] CreateAccountDto createAccountDto)
         {
             try
             {
-                if (!ModelState.IsValid)
-                {
-                    return BadRequest(ModelState);
-                }
                 var account = createAccountDto.ToAccount();
 
                 var result = await _accountRepository.CreateAsync(account, createAccountDto.Password);
@@ -35,21 +33,14 @@ namespace api.Controllers
                 {
                     var accountDto = account.ToAccountDto();
 
-                    return Ok
-                    (
-                        new
-                        {
-                            Message = "Account created successfully.",
-                            Account = accountDto
-                        }
-                    );
+                    return Ok(new DataResponse<AccountDto>("Account created successfully.", accountDto));
                 }
 
-                return BadRequest(result.Errors);
+                return BadRequest(new DataResponse<IEnumerable<string>>("Error creating account.", result.Errors.Select(e => e.Description)));
             }
             catch (Exception ex)
             {
-                return StatusCode(500, $"Internal server error: {ex.Message}");
+                return StatusCode(500, new MessageResponse($"Internal server error: {ex.Message}"));
             }
         }
     }
