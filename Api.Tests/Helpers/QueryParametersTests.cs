3613c977e2daaa4bcccc132a71b23198bd1860d7
Victor-MacielMF <joaovictormacieldefreitas@gmail.com>
2025-06-09 18:51:47 -0300

    test: add unit tests for QueryParameters default and custom values

---
+++ Api.Tests/Helpers/QueryParametersTests.cs
@@ -0,0 +1,34 @@
+﻿using api.Helpers;
+using FluentAssertions;
+
+namespace Api.Tests.Helpers
+{
+    public class QueryParametersTests
+    {
+        [Fact]
+        public void QueryParameters_ShouldHaveDefaultValues()
+        {
+            // Act
+            var query = new QueryParameters();
+
+            // Assert
+            query.PageNumber.Should().Be(1);
+            query.PageSize.Should().Be(20);
+        }
+
+        [Fact]
+        public void QueryParameters_ShouldAllowValueAssignment()
+        {
+            // Act
+            var query = new QueryParameters
+            {
+                PageNumber = 3,
+                PageSize = 50
+            };
+
+            // Assert
+            query.PageNumber.Should().Be(3);
+            query.PageSize.Should().Be(50);
+        }
+    }
+}
