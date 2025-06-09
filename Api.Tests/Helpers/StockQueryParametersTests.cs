01574b47da61f8a9236de965304d51b6c640511e
Victor-MacielMF <joaovictormacieldefreitas@gmail.com>
2025-06-09 18:52:52 -0300

    test: add unit tests for StockQueryParameters filters and sorting

---
+++ Api.Tests/Helpers/StockQueryParametersTests.cs
@@ -0,0 +1,46 @@
+﻿using api.Helpers;
+using FluentAssertions;
+
+namespace Api.Tests.Helpers
+{
+    public class StockQueryParametersTests
+    {
+        [Fact]
+        public void StockQueryParameters_ShouldHaveDefaultValues()
+        {
+            // Act
+            var query = new StockQueryParameters();
+
+            // Assert
+            query.PageNumber.Should().Be(1);
+            query.PageSize.Should().Be(20);
+            query.Symbol.Should().BeNull();
+            query.CompanyName.Should().BeNull();
+            query.SortBy.Should().BeNull();
+            query.IsAscending.Should().BeTrue();
+        }
+
+        [Fact]
+        public void StockQueryParameters_ShouldAllowValueAssignment()
+        {
+            // Act
+            var query = new StockQueryParameters
+            {
+                PageNumber = 2,
+                PageSize = 10,
+                Symbol = "AAPL",
+                CompanyName = "Apple",
+                SortBy = "MarketCap",
+                IsAscending = false
+            };
+
+            // Assert
+            query.PageNumber.Should().Be(2);
+            query.PageSize.Should().Be(10);
+            query.Symbol.Should().Be("AAPL");
+            query.CompanyName.Should().Be("Apple");
+            query.SortBy.Should().Be("MarketCap");
+            query.IsAscending.Should().BeFalse();
+        }
+    }
+}
